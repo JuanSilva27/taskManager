5af77c3a053a02372edb4f8edb3fc82753f20c64
Juan Silva <juan.silva.0270@gmail.com>
2025-03-24 14:33:12 -0300

    la vista editar muestra correctamente con los datos de la tarea a editar

--- client/src/components/FormTask.jsx
+++ client/src/components/FormTask.jsx
@@ -1,12 +1,12 @@
 import { Button } from './Button';
 import { useForm } from '../hooks/useForm';
 import useTasks from '../hooks/useTasks';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import { useEffect, useRef, useState } from 'react';
 
 export const FormTask = () => {
     const { storeTask, task, getTask } = useTasks()
-    const { formValues, handleInputChange, setFormValues } = useForm(
+    const { formValues, handleInputChange, setFormValues, reset } = useForm(
         {
             title: "",
             description: "",
@@ -16,11 +16,14 @@ export const FormTask = () => {
         }
     )
     const { id } = useParams()
+    const location = useLocation()
     let { title, description, createdAt, completed } = formValues
 
     useEffect(() => {
         if (id && Object.keys(task).length === 0) {
             getTask(id)
+        } else {
+            reset()
         }
     }, [id, getTask])
 
@@ -36,6 +39,12 @@ export const FormTask = () => {
             })
         }
     }, [task, setFormValues])
+
+    useEffect(() => {
+        if (!id) {
+            reset()
+        }
+    }, [location.pathname])
 
     const handleSubmit = (e) => {
 
@@ -72,7 +81,6 @@ export const FormTask = () => {
                     className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                     value={title}
                     name='title'
-                    /* ref={inputTitle} */
                     onChange={handleInputChange}
                 />
             </div>
@@ -93,7 +101,7 @@ export const FormTask = () => {
                     className='border w-full p-2 mt-2 placeholder-gray-400 rounded-md'
                     value={description}
                     name='description'
-                    /* ref={inputDescription} */
+
                     onChange={handleInputChange}
                 />
 
@@ -114,7 +122,7 @@ export const FormTask = () => {
                     className='border w-full p-2 mt-2 rounded-md'
                     value={createdAt}
                     name='createdAt'
-                    /* ref={inputCreatedAt} */
+
                     onChange={handleInputChange}
                 />
             </div>
