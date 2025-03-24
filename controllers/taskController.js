cc3f8c2c992ea1a69eb3e6f98de372ec20fb7f50
Juan Silva <juan.silva.0270@gmail.com>
2025-03-24 17:56:44 -0300

    se mueve el array tasks a un archivo propio

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,26 +1,4 @@
-let tasks=[{
-    id: 8,
-    title: "titulo8",
-    description: "SOY UNA TAREA",
-    completed: true,
-    createdAt: "1996-03-27"
-  },
-  {
-    id: 5,
-    title: "titulo5",
-    description: "SOY UNA TAREA",
-    completed: false,
-    createdAt: "2003-04-13"
-  },
-  {
-    id: 53,
-    title: "titulo53",
-    description: "SOY UNA TAREA",
-    completed: false,
-    createdAt: "2002-04-13"
-  },
-  
-]
+const tasks = require('../database/taskDB')
 
 module.exports = {
     list : (req, res) =>{
